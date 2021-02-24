6d85ead8fc6d8d1747007b155c5e7af6cc8ee07b
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-24 14:49:28 +0800

    Add tests for POST /jumplings

--- __tests__/jumplings.test.js
+++ __tests__/jumplings.test.js
@@ -3,7 +3,38 @@ const app = require("../src/app");
 
 describe("/jumplings", () => {
   it("should respond correctly to GET when there are no jumplings", async () => {
-    const response = await request(app).get("/jumplings").expect(200);
-    expect(response.body).toEqual([]);
+    const { body } = await request(app).get("/jumplings").expect(200);
+    expect(body).toEqual([]);
+  });
+
+  it("should respond correctly to POST with valid json", async () => {
+    const newJumpling = { name: "Jumpling" };
+    const { body } = await request(app)
+      .post("/jumplings")
+      .send(newJumpling)
+      .expect(201);
+    expect(body).toMatchObject(newJumpling);
+    const { allJumplings } = await request(app).get("/jumplings");
+    expect(allJumplings.length).toEqual(1);
+  });
+
+  it("should reject POST requests with invalid names", async () => {
+    const newJumpling = { name: 12345 };
+    const { body } = await request(app)
+      .post("/jumplings")
+      .send(newJumpling)
+      .expect(422);
+  });
+
+  it("should reject POST requests without names", async () => {
+    const newJumpling = { nickname: "Joey" };
+    const { body } = await request(app)
+      .post("/jumplings")
+      .send(newJumpling)
+      .expect(422);
+  });
+
+  it("should reject POST requests without json", async () => {
+    const response = await request(app).post("/jumplings").expect(400);
   });
 });
