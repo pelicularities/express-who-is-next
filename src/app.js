1a1505c35729275a9158d7dcba595e92428ac9af
Grace Teng <pelicularities@users.noreply.github.com>
2021-02-24 14:17:45 +0800

    Add root route and response

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send({
+    0: "GET    /",
+    1: "GET    /jumplings",
+    2: "POST   /jumplings",
+    3: "GET    /jumplings/:name",
+    4: "PUT    /jumplings/:id",
+    5: "DELETE /jumplings/:id",
+    6: "---------------------------",
+    7: "GET    /jumplings/presenter",
+  });
+});
+
 module.exports = app;
